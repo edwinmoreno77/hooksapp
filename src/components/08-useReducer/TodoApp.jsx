98ebb3cd27284dfe2c758b8ff52e8d8ab10bfed8
Edwin Moreno <edwinmoreno77@gmail.com>
2022-06-22 19:49:55 -0400

    useTodo.js was created

--- src/components/08-useReducer/TodoApp.jsx
+++ src/components/08-useReducer/TodoApp.jsx
@@ -1,55 +1,16 @@
-import React, { useEffect } from 'react'
-import { useReducer } from 'react'
+import React from 'react'
 import { TodoAdd } from './TodoAdd'
 import { TodoList } from './TodoList'
-import { todoReducer } from './todoReducer'
+import { useTodo } from './useTodo'
 
-const initialState = []
-
-const init = () => {
-    return JSON.parse(localStorage.getItem('todos')) || initialState
-}
 
 export const TodoApp = () => {
 
-    const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+    const { todos, todosCount, pendingTodosCount, handleDeleteTodo, handleToggleTodo, handleNewTodo } = useTodo()
 
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos));
-
-    }, [todos])
-
-
-    const handleNewTodo = (todo) => {
-
-        const action = {
-            type: '[TODO] Add Todo',
-            payload: todo,
-        }
-        dispatchTodo(action);
-
-    }
-
-    const handleDeleteTodo = (id) => {
-        const action = {
-            type: '[TODO] Remove Todo',
-            payload: id,
-        }
-        dispatchTodo(action);
-    }
-
-    const handleToggleTodo = (id) => {
-        const action = {
-            type: '[TODO] Toggle Todo',
-            payload: id,
-        }
-        dispatchTodo(action);
-    }
-
-    console.log(todos);
     return (
         <div className='container m-5'>
-            <h1>TodoApp: 10 <small>Pendientes: 2</small> </h1>
+            <h1>TodoApp: {todosCount} <small>Pendientes: {pendingTodosCount}</small> </h1>
             <hr />
 
             <div className="row">
--- src/components/08-useReducer/useTodo.js
+++ src/components/08-useReducer/useTodo.js
@@ -1,10 +1,57 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "./todoReducer"
 
 
+const initialState = []
 
-
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || initialState
+}
 
 export const useTodo = () => {
 
+    const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
 
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+    }, [todos])
+
+    const handleNewTodo = (todo) => {
+
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo,
+        }
+        dispatchTodo(action);
+
+    }
+
+    const handleDeleteTodo = (id) => {
+        const action = {
+            type: '[TODO] Remove Todo',
+            payload: id,
+        }
+        dispatchTodo(action);
+    }
+
+    const handleToggleTodo = (id) => {
+        const action = {
+            type: '[TODO] Toggle Todo',
+            payload: id,
+        }
+        dispatchTodo(action);
+    }
+
+    return {
+        pendingTodosCount: todos.filter(todo => !todo.done).length,
+        todos,
+        initialState,
+        init,
+        handleDeleteTodo,
+        handleToggleTodo,
+        handleNewTodo,
+        todosCount: todos.length
+    }
 
 }
