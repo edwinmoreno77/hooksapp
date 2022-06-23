959c15d5e6dade6ddbaf453bff20247c71341438
Edwin Moreno <edwinmoreno77@gmail.com>
2022-06-23 15:09:33 -0400

    useContent was created

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
+// import { TodoApp } from './components/08-useReducer/TodoApp';
 // import { Padre } from './components/07-tarea-memo/Padre';
 // import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { MemoHook } from './components/06-memos/MemoHook';
@@ -18,4 +19,4 @@ import { TodoApp } from './components/08-useReducer/TodoApp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<TodoApp />);
+root.render(<MainApp />);
